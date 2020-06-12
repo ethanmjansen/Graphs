9f47b95cf88516ebaf9b7b6754391ed8cab625f4
ethanmjansen <ethanmjansen@users.noreply.github.com>
2020-06-12 10:28:21 -0600

    MVP for Sprint

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -5,6 +5,8 @@ from world import World
 import random
 from ast import literal_eval
 
+from collections import deque
+
 class Stack():
     def __init__(self):
         self.stack = []
@@ -40,22 +42,82 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
 
-order = Stack()
-order.push(player.current_room)
 
-visited = {}
+"""'''visited = {}
 
-previous_room = None
+previous_room = 0
 
 def moving():
+    global previous_room
     direction = {}
+    current_room = player.current_room.id
     for i in player.current_room.get_exits():
         direction[i] = '?'
         visited[player.current_room.id] = direction
 
+    print(current_room, previous_room)
+    previous_room = current_room'''
+
+my_graph = dict(room_graph)
+
+for k, v in my_graph.items():
+    del v[0]
     
+
+def search(start_point):
+    search_queue = []
+    search_queue.append([i for i in my_graph[start_point][0]])
+    print(search_queue)
+    searched = []
+    while len(searched) != 500:
+        room = search_queue.pop()
+        if not room in searched:
+            search_queue.append(my_graph[room][0])
+            searched.append(room)
+
+    return searched
+
+my_list = search(player.current_room.id)"""
+
+def bfs(start):
+    traversal_path = []
+    opposites = {'n': 's', 's': 'n', 'w': 'e', 'e': 'w'}
+    order = Stack()
+
+    visited = {}
+    order.push((start, None))
+
+    while order.size() > 0:
+        current = order.stack[-1]
+        room = current[0]
+        direction_taken = current[1]
+
+        if room.id not in visited:
+            visited[room.id] = set()
+
+        if direction_taken:
+            visited[room.id].add(direction_taken)
+        
+        unexplored = [] 
+
+        for i in room.get_exits():
+            if i not in visited[room.id]:
+                unexplored.append(i)
+
+        if len(unexplored_paths) > 0:
+            direction = random.choice(unexplored_paths)
+            visited[room.id].add(direction)
+            order.push((room.get_room_in_direction(direction), opposites[direction]))
+            traversal_path.append(direction)
+        else:
+            traversal_path.append(direction_taken)
+            order.pop()
+
+    return traversal_path
+
+traversal_path = bfs(player.current_room)
+
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
@@ -76,16 +138,16 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
-
-    moving()
-    for k, v in visited.items():
-        print(k, v)
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
+# 
+#     moving()
+#     for k, v in visited.items():
+#         print(k, v)
